4b93be3fc29353b5b211ddfe286cc921e9e380e5
Frank Stein <dr.frank.stain@gmail.com>
2016-11-04 17:00:35 +0200

    Static string type.

---
+++ include/jnipp/utils/StaticString.h
@@ -0,0 +1,23 @@
+// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
+// Apache 2.0 License
+#pragma once
+
+
+namespace jnipp
+{
+	/**
+		@brief	Compile-time string. While runtime instances of one will be represented by plain strings.
+		@note	Using of compile-time strings for deducing signatures of functions save runtime performance.
+		`StaicString` is represented by variadic number of chars. In this form it's much easier to combine such strings.
+	*/
+	template< char... CHARS >
+	struct StaticString final
+	{
+		/// @brief	Get the plain string from stored chars.
+		inline static const char* GetString()
+		{
+			static const char string[] = { CHARS..., 0 };
+			return string;
+		};
+	};
+};
