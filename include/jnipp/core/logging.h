58c0fb1f33ad29e3bbf787351ebf4418cf811d48
Frank Stein <dr.frank.stain@gmail.com>
2016-11-06 23:16:46 +0200

    Logging changed to use variadic templates.

--- include/jnipp/core/logging.h
+++ include/jnipp/core/logging.h
@@ -12,113 +12,106 @@ namespace logging
 	/**
 		@brief	Write arbitrary message into android log-cat.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
 		@param	message_type	Type of message. `ANDROID_LOG_*` values allowed.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Message( const int32_t message_type, const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Message( const int32_t message_type, const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( message_type, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( message_type, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 
 	/**
 		@brief	Write message into android log-cat with `VERBOSE` type.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Verbose( const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Verbose( const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( ANDROID_LOG_VERBOSE, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( ANDROID_LOG_VERBOSE, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 
 	/**
 		@brief	Write message into android log-cat with `DEBUG` type.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Debug( const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Debug( const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( ANDROID_LOG_DEBUG, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( ANDROID_LOG_DEBUG, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 
 	/**
 		@brief	Write message into android log-cat with `INFO` type.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Info( const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Info( const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( ANDROID_LOG_INFO, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( ANDROID_LOG_INFO, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 
 	/**
 		@brief	Write message into android log-cat with `WARNING` type.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Warning( const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Warning( const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( ANDROID_LOG_WARN, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( ANDROID_LOG_WARN, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 
 	/**
 		@brief	Write message into android log-cat with `ERROR` type.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Error( const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Error( const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( ANDROID_LOG_ERROR, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( ANDROID_LOG_ERROR, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 
 	/**
 		@brief	Write message into android log-cat with `ASSERT` type.
 		@note	This function makes stuff only in debug build or if `JNIPP_LOGS_ENABLED` is set to `1`.
-		@param	format			Format of message.
-		@param	...				Arguments of message to be formated. Only plain types allowed.
+		@tparam	TArgumentTypes	Parameter pack of types of message arguments.
+		@param	format			Message format.
+		@param	arguments		List of message arguments to be formated. Only plain types allowed.
 	*/
-	inline void Assert( const char* format, ... )
+	template< typename... TArgumentTypes >
+	inline void Assert( const char* format, TArgumentTypes... arguments )
 	{
-		#if( ( defined( JNIPP_LOGS_ENABLED ) and JNIPP_LOGS_ENABLED ) || JNIPP_DEBUG_BUILD )
-		va_list arguments;
-		va_start( arguments, format );
-		__android_log_vprint( ANDROID_LOG_FATAL, TAG, format, arguments );
-		va_end( arguments );
+		#if( JNIPP_LOGS_ENABLED || JNIPP_DEBUG_BUILD )
+		__android_log_print( ANDROID_LOG_FATAL, TAG, format, std::forward<TArgumentTypes>( arguments )... );
 		#endif
 	};
 };
