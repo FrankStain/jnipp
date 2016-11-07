d9f5bdf6321540c77a00dbd5e56a9311c29a106c
Frank Stein <dr.frank.stain@gmail.com>
2016-11-07 22:05:41 +0200

    Interface of Field handle.

--- include/jnipp/handles/FieldHandle.h
+++ include/jnipp/handles/FieldHandle.h
@@ -5,5 +5,63 @@
 
 namespace jnipp
 {
-	
+	/// @brief	
+	template< typename TNativeType >
+	class FieldHandle final
+	{
+		friend class JniEnv;
+	public:
+		FieldHandle() = delete;
+		FieldHandle( const FieldHandle& other );
+		FieldHandle( FieldHandle&& other );
+		FieldHandle( const std::string& class_name, const std::string& field_name );
+		FieldHandle( const ClassHandle& class_handle, const std::string& field_name );
+		FieldHandle( const char* class_name, const char* field_name );
+		FieldHandle( const ClassHandle& class_handle, const char* field_name );
+
+		
+		/// @brief	Check the class handle carries valid value.
+		inline const bool IsValid() const				{ return m_field_id != 0; };
+
+		/// @brief	Get the value of field from given handle to object.
+		inline const bool GetValue( const ObjectHandle& object_handle, TFieldType& value_storage ) const;
+		
+		/// @brief	Get the value of field from given object ref.
+		inline const bool GetValue( jobject object_ref, TFieldType& value_storage ) const;
+
+		
+		/// @brief	Get the value of field from given handle to object.
+		inline const bool SetValue( const ObjectHandle& object_handle, TFieldType& value_storage ) const;
+		
+		/// @brief	Get the value of field from given object ref.
+		inline const bool SetValue( jobject object_ref, TFieldType& value_storage ) const;
+
+		
+		inline explicit operator const bool () const	{ return IsValid(); };
+
+	private:
+		using JavaType = typename marshaling::JniEnvFacade<TNativeType>::JavaType;
+
+		FieldHandle( jclass class_ref, const char* field_name );
+
+		
+		/// @brief	Get the value of field from given handle to object.
+		inline const bool GetValue( JNIEnv* local_env, const ObjectHandle& object_handle, TFieldType& value_storage ) const;
+		
+		/// @brief	Get the value of field from given object ref.
+		inline const bool GetValue( JNIEnv* local_env, jobject object_ref, TFieldType& value_storage ) const;
+
+		
+		/// @brief	Get the value of field from given handle to object.
+		inline const bool SetValue( JNIEnv* local_env, const ObjectHandle& object_handle, TFieldType& value_storage ) const;
+		
+		/// @brief	Get the value of field from given object ref.
+		inline const bool SetValue( JNIEnv* local_env, jobject object_ref, TFieldType& value_storage ) const;
+
+	private:
+		static constexpr auto FIELD_READ_HANDLER	= marshaling::JniEnvFacade<TNativeType>::FIELD_READ_HANDLER;
+		static constexpr auto FIELD_WRITE_HANDLER	= marshaling::JniEnvFacade<TNativeType>::FIELD_WRITE_HANDLER;
+
+		jfieldID	m_field_id	= 0;	// Field id for JNI.
+	};
 };
