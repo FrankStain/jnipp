bc206f4c6e96aaf2f9f96f4e49064ad7a515f8f5
Frank Stein <dr.frank.stain@gmail.com>
2016-11-14 21:57:01 +0200

    First code of handle to static function.

--- include/jnipp/handles/StaticFunctionHandle.h
+++ include/jnipp/handles/StaticFunctionHandle.h
@@ -6,10 +6,34 @@
 namespace jnipp
 {
 	template< typename TNativeReturnType, typename... TNativeArguments >
-	class FunctionHandle final
+	class StaticFunctionHandle final
 	{
 		friend class JniEnv;
 	public:
+		StaticFunctionHandle() = delete;
+		StaticFunctionHandle( const StaticFunctionHandle& other );
+		StaticFunctionHandle( StaticFunctionHandle&& other );
+		StaticFunctionHandle( const std::string& class_name, const std::string& function_name );
+		StaticFunctionHandle( const ClassHandle& class_handle, const std::string& function_name );
+		StaticFunctionHandle( const char* class_name, const char* function_name );
+		StaticFunctionHandle( const ClassHandle& class_handle, const char* function_name );
+
+
+		/// @brief	Check the field handle carries valid value.
+		inline const bool IsValid() const				{ return m_class_handle.IsValid() && ( m_function_id != 0 ); };
+
+		/// @brief	Call the function with given arguments.
+		inline TNativeReturnType Call( const ObjectHandle& object_handle, const TNativeArguments&... arguments ) const;
+
+
+		inline explicit operator const bool () const	{ return IsValid(); };
+
+	private:
+		using Signature		= FunctionSignature<TNativeReturnType, TNativeArguments...>;
+		using CallDecorator	= utils::StaticFunctionCall<TNativeReturnType, TNativeArguments...>;
+
+		/// @brief	Call the function with given arguments.
+		inline TNativeReturnType Call( JNIEnv* local_env, const TNativeArguments&... arguments ) const;
 
 	private:
 		ClassHandle	m_class_handle;			// Handle to class owning the static method.
