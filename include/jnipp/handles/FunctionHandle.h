6ea78ce71edad5d7f3392b22623cad1234fc66d9
Frank Stein <dr.frank.stain@gmail.com>
2016-12-10 14:45:01 +0200

    Function handle may be defaultly constructed and assigned afterwards.

--- include/jnipp/handles/FunctionHandle.h
+++ include/jnipp/handles/FunctionHandle.h
@@ -11,7 +11,7 @@ namespace jnipp
 	{
 		friend class JniEnv;
 	public:
-		FunctionHandle() = delete;
+		FunctionHandle() = default;
 		FunctionHandle( const FunctionHandle& other );
 		FunctionHandle( FunctionHandle&& other );
 		FunctionHandle( const std::string& class_name, const std::string& function_name );
@@ -19,12 +19,6 @@ namespace jnipp
 		FunctionHandle( const char* class_name, const char* function_name );
 		FunctionHandle( const ClassHandle& class_handle, const char* function_name );
 
-
-		/// @brief	Check the field handle carries valid value.
-		inline const bool IsValid() const				{ return m_function_id != 0; };
-
-		/// @brief	Get the JNI id of Java object method.
-		inline jmethodID GetFunctionId() const			{ return m_function_id; };
 
 		/// @brief	Call the function with given arguments for given Java object and return result.
 		inline TNativeReturnType Call( const ObjectHandle& object_handle, const TNativeArguments&... arguments ) const;
@@ -38,12 +32,23 @@ namespace jnipp
 		/// @brief	Call the function with given arguments for given Java object and return result.
 		inline TNativeReturnType CallNonVirtual( jobject object_ref, const TNativeArguments&... arguments ) const;
 
+
+		/// @brief	Check the field handle carries valid value.
+		inline const bool IsValid() const										{ return m_function_id != 0; };
+
+		/// @brief	Get the JNI id of Java object method.
+		inline jmethodID GetFunctionId() const									{ return m_function_id; };
+
 		/// @brief	Get the function signature string.
-		inline const char* GetSignature() const			{ return Signature::GetString(); };
+		inline const char* GetSignature() const									{ return Signature::GetString(); };
 
 
-		inline explicit operator const bool () const	{ return IsValid(); };
-		inline jmethodID operator * () const			{ return GetFunctionId(); };
+		inline const FunctionHandle& operator = ( const FunctionHandle& other )	{ m_function_id = other.m_function_id; return *this; };
+		inline const FunctionHandle& operator = ( FunctionHandle&& other )		{ m_function_id = std::move( other.m_function_id ); return *this; };
+
+
+		inline explicit operator const bool () const							{ return IsValid(); };
+		inline jmethodID operator * () const									{ return GetFunctionId(); };
 
 	private:
 		using Signature		= FunctionSignature< marshaling::TypeSignature<TNativeReturnType>, marshaling::TypeSignature<TNativeArguments>... >;
