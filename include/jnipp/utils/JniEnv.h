5de8666acbebd8c9de5f4e0141a94fe4727d157e
Frank Stein <dr.frank.stain@gmail.com>
2016-12-21 23:08:53 +0200

    Jni++ Environment now uses SFINAE and implicit argument conversion.

--- include/jnipp/utils/JniEnv.h
+++ include/jnipp/utils/JniEnv.h
@@ -15,53 +15,72 @@ namespace Jni
 	class JniEnv final
 	{
 	public:
+		template< typename TDesiredType, typename TGivenType >
+		using EnableConvertible = typename std::enable_if<std::is_convertible<TGivenType, TDesiredType>::value>::type;
+
+		template< bool... FLAGS >
+		struct FlagsContainer;
+
+		template< bool... FLAGS >
+		using TrueFlags = std::is_same< FlagsContainer<FLAGS..., true>, FlagsContainer<true, FLAGS...> >;
+
+	public:
 		JniEnv() = default;
 
 		/// @brief	Get the value of field belongs to object using the local JNI environment.
-		template< typename TNativeType >
+		template< typename TNativeType, typename TValueType, typename = EnableConvertible<TNativeType, TValueType> >
 		inline const bool GetValue(
 			const FieldHandle<TNativeType>& field_handle,
 			const ObjectHandle& object_handle,
-			TNativeType& value_storage
+			TValueType& value_storage
 		) const;
 
 		/// @brief	Get the value of static field using the local JNI environment.
-		template< typename TNativeType >
-		inline const bool GetValue( const StaticFieldHandle<TNativeType>& field_handle, TNativeType& value_storage ) const;
+		template< typename TNativeType, typename TValueType, typename = EnableConvertible<TNativeType, TValueType> >
+		inline const bool GetValue( const StaticFieldHandle<TNativeType>& field_handle, TValueType& value_storage ) const;
 
 		/// @brief	Set the value of field belongs to object using the local JNI environment.
-		template< typename TNativeType >
+		template< typename TNativeType, typename TValueType, typename = EnableConvertible<TNativeType, TValueType> >
 		inline const bool SetValue(
 			const FieldHandle<TNativeType>& field_handle,
 			const ObjectHandle& object_handle,
-			const TNativeType& value_storage
+			const TValueType& value_storage
 		) const;
 
 		/// @brief	Set the value of static field using the local JNI environment.
-		template< typename TNativeType >
-		inline const bool SetValue( const StaticFieldHandle<TNativeType>& field_handle, const TNativeType& value_storage ) const;
+		template< typename TNativeType, typename TValueType, typename = EnableConvertible<TNativeType, TValueType> >
+		inline const bool SetValue( const StaticFieldHandle<TNativeType>& field_handle, const TValueType& value_storage ) const;
 
 		/// @brief	Call the member function on object using the local JNI environment.
-		template< typename TNativeReturnType, typename... TNativeArguments >
+		template<
+			typename TNativeReturnType, typename... TNativeArguments, typename... TValueArguments,
+			typename = typename std::enable_if< TrueFlags< std::is_convertible<TValueArguments, TNativeArguments>::value... >::value >::type
+		>
 		inline TNativeReturnType Call(
 			const FunctionHandle<TNativeReturnType, TNativeArguments...>& function_handle,
 			const ObjectHandle& object_handle,
-			const TNativeArguments&... arguments
+			const TValueArguments&... arguments
 		);
 
 		/// @brief	Call the member function non-virtually on object using the local JNI environment.
-		template< typename TNativeReturnType, typename... TNativeArguments >
+		template<
+			typename TNativeReturnType, typename... TNativeArguments, typename... TValueArguments,
+			typename = typename std::enable_if<TrueFlags< std::is_convertible<TValueArguments, TNativeArguments>::value... >::value>::type
+		>
 		inline TNativeReturnType CallNonVirtual(
 			const FunctionHandle<TNativeReturnType, TNativeArguments...>& function_handle,
 			const ObjectHandle& object_handle,
-			const TNativeArguments&... arguments
+			const TValueArguments&... arguments
 		);
 
 		/// @brief	Call the static function using the local JNI environment.
-		template< typename TNativeReturnType, typename... TNativeArguments >
+		template<
+			typename TNativeReturnType, typename... TNativeArguments, typename... TValueArguments,
+			typename = typename std::enable_if<TrueFlags< std::is_convertible<TValueArguments, TNativeArguments>::value... >::value>::type
+		>
 		inline TNativeReturnType Call(
 			const StaticFunctionHandle<TNativeReturnType, TNativeArguments...>& function_handle,
-			const TNativeArguments&... arguments
+			const TValueArguments&... arguments
 		);
 
 		inline const bool IsValid() const				{ return m_local_env != nullptr; };
--- include/jnipp/utils/JniEnv.inl
+++ include/jnipp/utils/JniEnv.inl
@@ -5,62 +5,62 @@
 
 namespace Jni
 {
-	template< typename TNativeType >
+	template< typename TNativeType, typename TValueType, typename >
 	inline const bool JniEnv::GetValue(
 		const FieldHandle<TNativeType>& field_handle,
 		const ObjectHandle& object_handle,
-		TNativeType& value_storage
+		TValueType& value_storage
 	) const
 	{
 		return field_handle.GetValue( m_local_env, object_handle, value_storage );
 	};
 
-	template< typename TNativeType >
-	inline const bool JniEnv::GetValue( const StaticFieldHandle<TNativeType>& field_handle, TNativeType& value_storage ) const
+	template< typename TNativeType, typename TValueType, typename >
+	inline const bool JniEnv::GetValue( const StaticFieldHandle<TNativeType>& field_handle, TValueType& value_storage ) const
 	{
 		return field_handle.GetValue( m_local_env, value_storage );
 	};
 
-	template< typename TNativeType >
+	template< typename TNativeType, typename TValueType, typename >
 	inline const bool JniEnv::SetValue(
 		const FieldHandle<TNativeType>& field_handle,
 		const ObjectHandle& object_handle,
-		const TNativeType& value_storage
+		const TValueType& value_storage
 	) const
 	{
 		return field_handle.SetValue( m_local_env, object_handle, value_storage );
 	};
 
-	template< typename TNativeType >
-	inline const bool JniEnv::SetValue( const StaticFieldHandle<TNativeType>& field_handle, const TNativeType& value_storage ) const
+	template< typename TNativeType, typename TValueType, typename >
+	inline const bool JniEnv::SetValue( const StaticFieldHandle<TNativeType>& field_handle, const TValueType& value_storage ) const
 	{
 		return field_handle.SetValue( m_local_env, value_storage );
 	};
 
-	template< typename TNativeReturnType, typename... TNativeArguments >
+	template< typename TNativeReturnType, typename... TNativeArguments, typename... TValueArguments, typename >
 	inline TNativeReturnType JniEnv::Call(
 		const FunctionHandle<TNativeReturnType, TNativeArguments...>& function_handle,
 		const ObjectHandle& object_handle,
-		const TNativeArguments&... arguments
+		const TValueArguments&... arguments
 	)
 	{
 		return function_handle.Call( m_local_env, object_handle, arguments... );
 	};
 
-	template< typename TNativeReturnType, typename... TNativeArguments >
+	template< typename TNativeReturnType, typename... TNativeArguments, typename... TValueArguments, typename >
 	inline TNativeReturnType JniEnv::CallNonVirtual(
 		const FunctionHandle<TNativeReturnType, TNativeArguments...>& function_handle,
 		const ObjectHandle& object_handle,
-		const TNativeArguments&... arguments
+		const TValueArguments&... arguments
 	)
 	{
 		return function_handle.CallNonVirtual( m_local_env, object_handle, arguments... );
 	};
 
-	template< typename TNativeReturnType, typename... TNativeArguments >
+	template< typename TNativeReturnType, typename... TNativeArguments, typename... TValueArguments, typename >
 	inline TNativeReturnType JniEnv::Call(
 		const StaticFunctionHandle<TNativeReturnType, TNativeArguments...>& function_handle,
-		const TNativeArguments&... arguments
+		const TValueArguments&... arguments
 	)
 	{
 		return function_handle.Call( m_local_env, arguments... );
