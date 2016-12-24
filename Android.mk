bd61f04ad3e696979e7d321581cecd98c5556067
Frank Stein <dr.frank.stain@gmail.com>
2016-12-24 18:42:55 +0200

    Android.mk reformated.

--- Android.mk
+++ Android.mk
@@ -15,12 +15,14 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE			:= jnipp
 LOCAL_EXPORT_C_INCLUDES	:= $(PROJECT_PATH)/include
+LOCAL_EXPORT_CPPFLAGS	:= -Wno-format-security
+LOCAL_EXPORT_LDLIBS		:= -latomic -llog
+
 LOCAL_C_INCLUDES		:= $(LOCAL_EXPORT_C_INCLUDES)
 LOCAL_SRC_FILES			:= $(patsubst $(LOCAL_PATH)/%, %, $(filter %.cpp, $(call lookup-files, $(LOCAL_PATH))))
+LOCAL_CPPFLAGS			:= -std=c++11 $(LOCAL_EXPORT_CPPFLAGS)
 LOCAL_CPP_FEATURES		:= rtti exceptions
-LOCAL_EXPORT_LDLIBS		:= -latomic -llog
-LOCAL_CPPFLAGS			:= -std=c++11 -Wno-format-security
-LOCAL_EXPORT_CPPFLAGS	:= -Wno-format-security
 
-# Build rules for Android native applications.
+
+# Build rules for static library.
 include $(BUILD_STATIC_LIBRARY)
