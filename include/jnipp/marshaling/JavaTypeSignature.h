c859ad758477da06b23a67aaa07a0a1d1e88398f
Frank Stein <dr.frank.stain@gmail.com>
2016-12-27 20:09:20 +0200

    Signatures specification for regular Java types.

--- include/jnipp/marshaling/JavaTypeSignature.h
+++ include/jnipp/marshaling/JavaTypeSignature.h
@@ -7,6 +7,87 @@ namespace Jni
 {
 namespace Marshaling
 {
+	template< typename TJavaType >
+	struct JavaTypeTraits;
+
 	
+	template< typename TJavaType >
+	using JavaTypeSignature	= typename JavaTypeTraits<TJavaType>::Signature;
+
+	template< typename TJavaReturnType, typename... TJavaArgumentTypes >
+	using JavaFunctionSignature	= FunctionSignature<JavaTypeSignature<TJavaReturnType>, JavaTypeSignature<TJavaArgumentTypes>...>;
+
+
+	template<>
+	struct JavaTypeTraits<void>
+	{
+		using Signature	= StaticString<'V'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jboolean>
+	{
+		using Signature	= StaticString<'Z'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jbyte>
+	{
+		using Signature	= StaticString<'B'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jchar>
+	{
+		using Signature	= StaticString<'C'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jshort>
+	{
+		using Signature	= StaticString<'S'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jint>
+	{
+		using Signature	= StaticString<'I'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jlong>
+	{
+		using Signature	= StaticString<'J'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jfloat>
+	{
+		using Signature	= StaticString<'F'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jdouble>
+	{
+		using Signature	= StaticString<'D'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jobject>
+	{
+		using Signature	= ClassName<'j', 'a', 'v', 'a', '/', 'l', 'a', 'n', 'g', '/', 'O', 'b', 'j', 'e', 'c', 't'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jclass>
+	{
+		using Signature	= ClassName<'j', 'a', 'v', 'a', '/', 'l', 'a', 'n', 'g', '/', 'C', 'l', 'a', 's', 's'>;
+	};
+
+	template<>
+	struct JavaTypeTraits<jstring>
+	{
+		using Signature	= ClassName<'j', 'a', 'v', 'a', '/', 'l', 'a', 'n', 'g', '/', 'S', 't', 'r', 'i', 'n', 'g'>;
+	};
 };
 };
