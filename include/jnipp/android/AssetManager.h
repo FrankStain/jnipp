07794de6a0be6ec13f7e9f80b4f0d482599be97b
Frank Stein <dr.frank.stain@gmail.com>
2017-07-17 19:13:18 +0200

    Stubs for Asset Manager object.

---
+++ include/jnipp/android/AssetManager.h
@@ -0,0 +1,17 @@
+// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
+// Apache 2.0 License
+#pragma once
+
+#include <jnipp/jnipp.h>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+#include "AssetManager.const.h"
+
+
+namespace Jni
+{
+namespace Android
+{
+
+}
+}
