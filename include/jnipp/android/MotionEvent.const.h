80783ae433be939def0dbf4471556dceee773f5b
Frank Stein <dr.frank.stain@gmail.com>
2017-07-17 15:58:06 +0200

    Comments fixed.

--- include/jnipp/android/MotionEvent.const.h
+++ include/jnipp/android/MotionEvent.const.h
@@ -10,28 +10,28 @@ namespace Android
 	/// @brief	Translation of input device class.
 	enum class InputSourceClass : int32_t
 	{
-		None		= AINPUT_SOURCE_CLASS_NONE,			// https://developer.android.com/reference/android/view/InputDevice.html#SOURCE_CLASS_NONE
-		Button		= AINPUT_SOURCE_CLASS_BUTTON,		// https://developer.android.com/reference/android/view/InputDevice.html#SOURCE_CLASS_BUTTON
-		Pointer		= AINPUT_SOURCE_CLASS_POINTER,		// https://developer.android.com/reference/android/view/InputDevice.html#SOURCE_CLASS_POINTER
-		Navigation	= AINPUT_SOURCE_CLASS_NAVIGATION,	// https://developer.android.com/reference/android/view/InputDevice.html#SOURCE_CLASS_TRACKBALL
-		Position	= AINPUT_SOURCE_CLASS_POSITION,		// https://developer.android.com/reference/android/view/InputDevice.html#SOURCE_CLASS_POSITION
-		Joystick	= AINPUT_SOURCE_CLASS_JOYSTICK,		// https://developer.android.com/reference/android/view/InputDevice.html#SOURCE_CLASS_JOYSTICK
+		None		= AINPUT_SOURCE_CLASS_NONE,				// android.view.InputDevice.SOURCE_CLASS_NONE
+		Button		= AINPUT_SOURCE_CLASS_BUTTON,			// android.view.InputDevice.SOURCE_CLASS_BUTTON
+		Pointer		= AINPUT_SOURCE_CLASS_POINTER,			// android.view.InputDevice.SOURCE_CLASS_POINTER
+		Navigation	= AINPUT_SOURCE_CLASS_NAVIGATION,		// android.view.InputDevice.SOURCE_CLASS_TRACKBALL
+		Position	= AINPUT_SOURCE_CLASS_POSITION,			// android.view.InputDevice.SOURCE_CLASS_POSITION
+		Joystick	= AINPUT_SOURCE_CLASS_JOYSTICK,			// android.view.InputDevice.SOURCE_CLASS_JOYSTICK
 	};
 
 	/// @brief	Translation of motion actions.
 	enum class MotionActionType : int32_t
 	{
-		Down		= AMOTION_EVENT_ACTION_DOWN,
-		Up			= AMOTION_EVENT_ACTION_UP,
-		Move		= AMOTION_EVENT_ACTION_MOVE,
-		Cancel		= AMOTION_EVENT_ACTION_CANCEL,
-		Outside		= AMOTION_EVENT_ACTION_OUTSIDE,
-		PointerDown	= AMOTION_EVENT_ACTION_POINTER_DOWN,
-		PointerUp	= AMOTION_EVENT_ACTION_POINTER_UP,
-		HoverMve	= AMOTION_EVENT_ACTION_HOVER_MOVE,
-		Scroll		= AMOTION_EVENT_ACTION_SCROLL,
-		HoverEnter	= AMOTION_EVENT_ACTION_HOVER_ENTER,
-		HoverExit	= AMOTION_EVENT_ACTION_HOVER_EXIT,
+		Down		= AMOTION_EVENT_ACTION_DOWN,			// android.view.MotionEvent.ACTION_DOWN
+		Up			= AMOTION_EVENT_ACTION_UP,				// android.view.MotionEvent.ACTION_UP
+		Move		= AMOTION_EVENT_ACTION_MOVE,			// android.view.MotionEvent.ACTION_MOVE
+		Cancel		= AMOTION_EVENT_ACTION_CANCEL,			// android.view.MotionEvent.ACTION_CANCEL
+		Outside		= AMOTION_EVENT_ACTION_OUTSIDE,			// android.view.MotionEvent.ACTION_OUTSIDE
+		PointerDown	= AMOTION_EVENT_ACTION_POINTER_DOWN,	// android.view.MotionEvent.ACTION_POINTER_DOWN
+		PointerUp	= AMOTION_EVENT_ACTION_POINTER_UP,		// android.view.MotionEvent.ACTION_POINTER_UP
+		HoverMve	= AMOTION_EVENT_ACTION_HOVER_MOVE,		// android.view.MotionEvent.ACTION_HOVER_MOVE
+		Scroll		= AMOTION_EVENT_ACTION_SCROLL,			// android.view.MotionEvent.ACTION_SCROLL
+		HoverEnter	= AMOTION_EVENT_ACTION_HOVER_ENTER,		// android.view.MotionEvent.ACTION_HOVER_ENTER
+		HoverExit	= AMOTION_EVENT_ACTION_HOVER_EXIT,		// android.view.MotionEvent.ACTION_HOVER_EXIT
 	};
 }
 
