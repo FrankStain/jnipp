2a8e2722979abaea27e2c740fbf7d1fe9741bfb9
Frank Stein <dr.frank.stain@gmail.com>
2017-07-19 18:09:53 +0200

    Stub for java.util.Locale class representation.

---
+++ include/jnipp/android/Locale.h
@@ -0,0 +1,19 @@
+// Copyright since 2016 : Evgenii Shatunov (github.com/FrankStain/jnipp)
+// Apache 2.0 License
+#pragma once
+
+#include <jnipp/jnipp.h>
+
+
+namespace Jni
+{
+namespace Android
+{
+
+}
+
+namespace Marshaling
+{
+
+}
+}
